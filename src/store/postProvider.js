3107cbb85da9d174e4900bbec484c1d508f605bf
Kevin <kevin.manssat@hotmail.fr>
2019-07-20 15:29:57 +0200

    refetching after admin post action

--- src/store/postProvider.js
+++ src/store/postProvider.js
@@ -9,28 +9,25 @@ function PostProvider(props) {
   const [nbPostPerPage] = useState(6);
 
   useEffect(() => {
-    async function fetchData() {
-      const result = await PostService.getPost();
-
-      const posts = result.filter(({ type }) => type === 'post');
-      const pages = result.filter(({ type }) => type === 'page');
-
-      setPosts(posts);
-      setPages(pages);
-    }
     fetchData();
   }, []);
+
+  async function fetchData() {
+    const result = await PostService.getPost();
+
+    const posts = result.filter(({ type }) => type === 'post');
+    const pages = result.filter(({ type }) => type === 'page');
+
+    setPosts(posts);
+    setPages(pages);
+  }
 
   async function deletePost(id) {
     const result = await PostService.deletePost(id);
 
     if (!result._id) return;
 
-    const filteredPosts = posts.filter(({ _id }) => _id !== id);
-    const filteredPages = pages.filter(({ _id }) => _id !== id);
-
-    setPosts(filteredPosts);
-    setPages(filteredPages);
+    await fetchData();
   }
 
   async function addPost(post) {
@@ -38,30 +35,7 @@ function PostProvider(props) {
 
     if (!result._id) return;
 
-    let postArray = [];
-    let pageArray = [];
-    const postIndex = posts.findIndex((post) => post._id === result._id);
-    const pageIndex = pages.findIndex((page) => page._id === result._id);
-
-    if (result.type === 'post') {
-      if (postIndex === -1) {
-        postArray = [result, ...posts];
-      } else {
-        postArray = [...posts];
-        postArray[postIndex] = result;
-      }
-      setPosts(postArray);
-    }
-
-    if (result.type === 'page') {
-      if (pageIndex === -1) {
-        pageArray = [result, ...pages];
-      } else {
-        pageArray = [...pages];
-        pageArray[pageIndex] = result;
-      }
-      setPages(pageArray);
-    }
+    await fetchData();
   }
 
   return (
